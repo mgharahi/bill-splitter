495e9627bcd9d6e381fc3fdcf20cd6e065535988
mehdi-gharahi <m.gharahi@azkivam.com>
2025-09-22 23:14:36 +0330

    fix: add missed documents to transaction controller.

--- src/main/java/com/snapp/billsplitter/infrastructure/controller/TransactionController.java
+++ src/main/java/com/snapp/billsplitter/infrastructure/controller/TransactionController.java
@@ -2,10 +2,15 @@ package com.snapp.billsplitter.infrastructure.controller;
 
 import com.snapp.billsplitter.application.service.transaction.TransactionService;
 import com.snapp.billsplitter.application.service.transaction.dto.command.AddTransactionRequest;
+import com.snapp.billsplitter.infrastructure.controller.dto.auth.AuthRequest;
 import com.snapp.billsplitter.infrastructure.service.bucket4j.RateLimitService;
 import com.snapp.billsplitter.infrastructure.util.CurrentUser;
 import com.snapp.billsplitter.infrastructure.util.MessageHelper;
 import io.github.bucket4j.Bucket;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -23,12 +28,26 @@ public class TransactionController {
     private final RateLimitService rateLimitService;
     private final CurrentUser currentUser;
 
+    @Operation(
+            summary = "Add new transaction",
+            description = "Add new transaction to an existed event with some participants",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    required = true,
+                    description = "The transaction detail to save as new transaction",
+                    content = @Content(schema = @Schema(implementation = AddTransactionRequest.class))
+            )
+    )
     @PutMapping
     public ResponseEntity<?> addTransaction(@RequestBody AddTransactionRequest request) {
         transactionService.addTransaction(request);
         return ResponseEntity.ok("success");
     }
 
+
+    @Operation(
+            summary = "Get user balance",
+            description = "Get user balance as pdf file"
+    )
     @GetMapping("/report/balance")
     public ResponseEntity<byte[]> getBalanceReport() {
 
